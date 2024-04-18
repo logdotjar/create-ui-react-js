ed8eb84d2f4bd0805045400796db0e0933ce3f2f
jinareum <jinareum.76@gmail.com>
2024-04-18 22:51:19 +0900

    fix: gnb 생성전

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import './globals.scss'
-import Gnb from './gnb'
 
 export const metadata: Metadata = {
   title: 'UI요소모음 | FE재남',
@@ -11,7 +10,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ko">
       <body>
-        <Gnb />
         <main>{children}</main>
       </body>
     </html>
